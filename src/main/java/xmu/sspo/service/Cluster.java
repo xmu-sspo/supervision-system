022876cae04ca76b954216d093d3ac256ac67a91
heyingzhu <34204918+heyingzhu@users.noreply.github.com>
2018-11-18 21:06:07 +0800

    修改聚类定时

--- src/main/java/xmu/sspo/service/Cluster.java
+++ src/main/java/xmu/sspo/service/Cluster.java
@@ -14,79 +14,133 @@ import xmu.sspo.model.Topic;
 
 @Component
 public class Cluster {
-	//聚类产生话题，每天两次，6点和13点
-	@Scheduled(cron="0 0 6,13 * * ?")
-	public void TaskJob() {
-		System.out.println("cluster");
-		
-//		Process proc;
-//		String python_root_path = "D:\\莹\\大创\\舆情监督\\中文分词\\";
-//		String python_result_path = "C:\\Users\\lenovo\\Desktop\\";
-//		
-//		String[] indexs = new String[] {};
-//    	ArrayList<String> news_list = new ArrayList<String>();
-//    	ArrayList<String> topic_list = new ArrayList<String>(); 
-//    	
-//        try {
+	//对近30天的新闻进行聚类，一天一次，每天6点进行
+	@Scheduled(cron="0 0 6 * * ?")
+	public void month_Cluster() {
+		System.out.println("month_cluster");
+		String[] args1 = new String[] { "python", "D:\\莹\\大创\\舆情监督\\中文分词\\new_kmeans.py", String.valueOf(30)}; 
+
+        try {
 //            proc = Runtime.getRuntime().exec("python " + python_root_path + "new_kmeans.py");//运行python程序
-//            //读取python运行结果
-//            String line="";
-//            System.out.println("-------------python begin---------------");
-//            InputStreamReader isr = new InputStreamReader(new FileInputStream(python_result_path + "pyResult.txt"), "UTF-8");   //C:\\Users\\lenovo\\Desktop\\
-//            BufferedReader in = new BufferedReader(isr);
-//        	line=in.readLine();
-//        	System.out.println("-------------python end-----------------");
-//        	while (line!=null){
-////        		System.out.println(line);
-//        		if(line.startsWith("center_index:")){
-//        			line = in.readLine();
-//        			indexs = line.split(", ");
-//        			line = in.readLine();     			
-//        		}
-//        		if(line.startsWith("topic:")||line.startsWith("new_list:")){
-//					line = in.readLine();
-//				}else if (line.startsWith("[")) {
-//					news_list.add(line);
-//					line = in.readLine();
-//				}else {
-//					topic_list.add(line);
-//					line = in.readLine();
-//				}
-//        	}//end while
-//            in.close();
-//            proc.waitFor();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        } 
-//        
-//        //各种输出
-//        System.out.println("index:" + indexs.length);
-//        for(int i=0; i<indexs.length; i++) {
-//        	System.out.println(indexs[i]);
-//        }
-//        System.out.println("news_list:" + news_list.size());
-//        for(int i=0; i<indexs.length; i++) {
-//        	System.out.println(news_list.get(i));
-//        }
-//        System.out.println("topic:" + topic_list.size());
-//        for(int i=0; i<indexs.length; i++) {
-//        	System.out.println(topic_list.get(i));
-//        }
-//		
-//        //存储话题相关内容
-//        ArrayList<Topic> topicList = new ArrayList<Topic>();
-//        for(int i=0; i<indexs.length; i++) {
-//        	int index = Integer.parseInt(indexs[i].replaceAll("\\[|\\]| ", ""));
-//        	String title = topic_list.get(i);
-//        	String newsList = news_list.get(i).replaceAll("\\[|\\]| ", "");       	       		
-//        	Topic topic = new Topic(newsList, index, title); 
-//        	topicList.add(topic);
-//        }
-//        return topicList;
+        	Process proc=Runtime.getRuntime().exec(args1);
+          //用输入输出流来截取结果
+            BufferedReader in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+            String line = null;
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);
+            }
+            in.close();
+            proc.waitFor();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } 
+
 	}
 	
+	//对近7天的新闻进行聚类，一天一次，每天6点进行
+	@Scheduled(cron="0 0 6 * * ?")
+	public void week_Cluster() {
+		System.out.println("week_cluster");
+		String[] args1 = new String[] { "python", "D:\\莹\\大创\\舆情监督\\中文分词\\new_kmeans.py", String.valueOf(7)}; 
+
+        try {
+//            proc = Runtime.getRuntime().exec("python " + python_root_path + "new_kmeans.py");//运行python程序
+        	Process proc=Runtime.getRuntime().exec(args1);
+          //用输入输出流来截取结果
+            BufferedReader in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+            String line = null;
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);
+            }
+            in.close();
+            proc.waitFor();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } 
+
+	}
+	
+	//对近3天的新闻进行聚类，一天一次，每天6点进行
+	@Scheduled(cron="0 0 6 * * ?")
+	public void threedays_Cluster() {
+		System.out.println("threedays_cluster");
+		String[] args1 = new String[] { "python", "D:\\莹\\大创\\舆情监督\\中文分词\\new_kmeans.py", String.valueOf(3)}; 
+
+        try {
+//            proc = Runtime.getRuntime().exec("python " + python_root_path + "new_kmeans.py");//运行python程序
+        	Process proc=Runtime.getRuntime().exec(args1);
+          //用输入输出流来截取结果
+            BufferedReader in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+            String line = null;
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);
+            }
+            in.close();
+            proc.waitFor();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } 
+
+	}
+	
+	//对当天的新闻进行聚类，2分钟一次
+	@Scheduled(cron="0/120 * * * * ?")
+	public void today_Cluster() {
+		System.out.println("today_cluster");
+		String[] args1 = new String[] { "python", "D:\\莹\\大创\\舆情监督\\中文分词\\new_kmeans.py", String.valueOf(1)}; 
+
+        try {
+//            proc = Runtime.getRuntime().exec("python " + python_root_path + "new_kmeans.py");//运行python程序
+        	Process proc=Runtime.getRuntime().exec(args1);
+          //用输入输出流来截取结果
+            BufferedReader in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+            String line = null;
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);
+            }
+            in.close();
+            proc.waitFor();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } 
+
+	}
+	
+	
+	//测试
+//	@Scheduled(cron="0/5 * * * * ?")
+//	public void test_task() {
+//		System.out.println("test!");
+//		String path = this.getClass().getClassLoader().getResource(".").getPath();
+//		System.out.println(path);
+//		Process proc;
+//		String[] args1 = new String[] { "python", "test_1.py", String.valueOf(1)}; 
+//		
+//		        try {
+//		            proc = Runtime.getRuntime().exec("python " + path + "test_1.py");//运行python程序
+////		        	 proc=Runtime.getRuntime().exec(args1);
+//		          //用输入输出流来截取结果
+//		            BufferedReader in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+//		            String line = null;
+//		            while ((line = in.readLine()) != null) {
+//		                System.out.println(line);
+//		            }
+//		            in.close();
+//		            proc.waitFor();
+//		        } catch (IOException e) {
+//		            e.printStackTrace();
+//		        } catch (InterruptedException e) {
+//		            e.printStackTrace();
+//		        } 
+//	}
 	
 	
 }
