9d6bddd93e4b9597e09d88d98bc8cb57cc015037
qinlingyun <342637442@qq.com>
2018-11-24 22:16:50 +0800

    solr的controller

--- src/main/java/xmu/sspo/controller/SolrController.java
+++ src/main/java/xmu/sspo/controller/SolrController.java
@@ -1,13 +1,17 @@
 package xmu.sspo.controller;
 
-import org.apache.solr.client.solrj.SolrClient;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrDocumentList;
-import org.apache.solr.common.SolrInputDocument;
 import org.apache.solr.common.params.FacetParams;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,50 +19,45 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import xmu.sspo.dao.NewsDao;
 import xmu.sspo.model.News;
-import xmu.sspo.model.Text;
-import xmu.sspo.service.impl.SolrTest;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import xmu.sspo.model.Topic;
+import xmu.sspo.service.SolrService;
 
 @RestController
 public class SolrController {
 	
-	private final static String SOLR_URL = "http://localhost:8080/solr/";  
-	 
-    private String solrCore="articles";
-    
-//    @RequestMapping("/query")
-//    private ModelAndView querySolr() throws Exception {
-//    	HttpSolrClient solrServer = new HttpSolrClient(SOLR_URL+solrCore);  
-//        SolrQuery query = new SolrQuery();
-//        query.setQuery("content:*����* OR content:*�Ϻ�* ");
-//        query.setFacet(true);
-//        query.addFacetField("content","id");
-//        query.setFacetSort(FacetParams.FACET_SORT_COUNT);
-//        query.setFacetMinCount(1);
-//     //   query.setFacetMissing(false);ֵ
-//        
-//        QueryResponse response = solrServer.query(query);
-//        System.out.println(response.getFacetFields());
-//        SolrDocumentList solrDocumentList = response.getResults();  
-//        System.out.println("ͨsolrDocumentList:"+solrDocumentList.getNumFound()); 
-//        //
-//        for (SolrDocument doc : solrDocumentList) {
-//            System.out.println("id:"+doc.get("id")+"   title:"+doc.get("title")+"   content:"+doc.get("content"));
-//        } 
-//        List<News> tmpLists = response.getBeans(News.class);
-//        if(tmpLists!=null && tmpLists.size()>0){
-//            System.out.println("....."); 
-//            for(News per:tmpLists){
-//                System.out.println(per.toString());
-//            }
-//        }      
-//        ModelAndView newsView = new ModelAndView("news-list");	
-//		newsView.addObject("news", tmpLists);
-//		return newsView;
-//    }
+	@Autowired
+	private SolrService solrService;
+	
+	@Autowired
+	private NewsDao dao;
+	
+//	@RequestMapping("/test")
+//	@ResponseBody
+//	public List<News> getNewsList() {
+//		Map<String, Object> map = new HashMap<String, Object>();
+//		map.put("month", "201811");
+//		long month = 201811;
+//		List<News> topic_list = dao.listNewsPerMonth(map);
+//		return topic_list;
+//	}
+	
+	@RequestMapping("/query")
+	public ModelAndView querySolr() {
+		
+//		try {
+//			solrService.addIndex();
+//			System.out.println("添加索引成功");//动态索引
+//		} catch (SolrServerException e) {
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+		ModelAndView newsView = new ModelAndView("news-list");	
+		List<News> tmpLists = solrService.getSolrNews();
+		newsView.addObject("news", tmpLists);
+		return newsView;
+	}
+	
 }
